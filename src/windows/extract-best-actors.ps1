c5d0f0efde1b924d927a55545ee8b81372d95cf1
MatheusMaciel <teu.araujo@gmail.com>
2015-08-29 21:58:53 -0300

    oscar data parsing scripts

---
+++ src/windows/extract-best-actors.ps1
@@ -0,0 +1,24 @@
+﻿#$data =gc .\bestactors.txt | ConvertFrom-String -tf .\bestactors.template.txt
+
+$listOfYears = @()
+
+
+ foreach ($record in $data) {
+    
+ 
+    $nominees = $record.nominees
+    foreach ($nominee in $nominees) {
+        $nominee.movie = $nominee.movie -replace "(.)*\|(\'\'\[\[)?",""      
+    }
+        	 
+     $props = @{
+        year = $record.year
+        winner = $record.winner
+        winnerMovie = $record.winnerMovie -replace "(.)*\|(\'\'\[\[)?",""
+        nominees = $nominees       
+     }
+
+     $listOfYears += $props       
+}
+$json = $listOfYears | ConvertTo-Json
+$json | Out-File "oscar-best-actors.json"
---
+++ src/windows/extract-best-director.ps1
@@ -0,0 +1,24 @@
+﻿$data = gc bestdirector.txt | ConvertFrom-String -tf bestdirector.template.txt
+
+$listOfYears = @()
+
+
+ foreach ($record in $data) {
+    
+ 
+    $nominees = $record.nomineesList.Items
+    foreach ($nominee in $nominees) {
+        $nominee.movie = $nominee.movie -replace "(.)*\|(\'\'\[\[)?",""      
+    }
+        	 
+     $props = @{
+        year = $record.year
+        winner = $record.best.name
+        winnerMovie = $record.best.movie -replace "(.)*\|(\'\'\[\[)?",""
+        nominees = $nominees       
+     }
+
+     $listOfYears += $props       
+}
+$json = $listOfYears | ConvertTo-Json
+$json | Out-File "oscar-best-director.json"
